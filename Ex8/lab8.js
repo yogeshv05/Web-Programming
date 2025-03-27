574708982bf48f91940d78c0a4621cbe14da96c0
yogeshv05 <yogesh.v2023@vitstudent.ac.in>
2025-03-27 19:15:18 +0530

    Create lab8.js

---
+++ Ex8/lab8.js
@@ -0,0 +1,5 @@
+function calc(){
+    a1 = parseInt(document.getElementById("a").value);
+    b1 = parseInt(document.getElementById("b").value);
+    document.getElementById("c").value = a1*10 + b1*5;
+}
